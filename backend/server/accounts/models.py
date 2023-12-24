db34df36a3165dfe19053181c47725f59ed00407
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-24 23:39:22 +0300

    user account model set up

--- backend/server/accounts/models.py
+++ backend/server/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth. models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserAccountsManager():
+    pass
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """This model overides basic user save"""
+    email = models.EmailField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountsManager()
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -13,3 +13,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, )
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
