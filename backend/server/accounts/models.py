c33f3ec7a8fbd18902388ac73016b8370c3777fa
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-24 23:50:51 +0300

    writes function to create a normal user

--- backend/server/accounts/models.py
+++ backend/server/accounts/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.contrib.auth. models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-class UserAccountsManager():
-    pass
+class UserAccountManager(BaseUserManager):
+    """functionality to create a normal user and admin user"""
+    def create_user(self, email, name, password=None):
+        """create a normal user"""
+        if not email:
+            raise ValueError("A user must have an email!")
+        
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     """This model overides basic user save"""
@@ -11,4 +25,4 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserAccountsManager()
+    objects = UserAccountManager()
