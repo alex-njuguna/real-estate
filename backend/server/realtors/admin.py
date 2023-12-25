555ef5abb807aa6a83556f0f9d455bb8631b10ab
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-25 13:23:55 +0300

    customizes admin panel for the realtor model

--- backend/server/realtors/admin.py
+++ backend/server/realtors/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Realtor
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    """fields to display in the admin panel for the Realtor model"""
+    list_display = ['id', 'name', 'phone', 'email', 'date_hired']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
+
+
