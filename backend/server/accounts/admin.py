23f30b298471eec08bbc616c9e43da115f708460
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-25 13:27:06 +0300

    customizes admin panel for the UserAccount model

--- backend/server/accounts/admin.py
+++ backend/server/accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import UserAccount
 
-admin.site.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    """customize display of fields in the UserAccount model"""
+    list_display = ['id', 'name', 'email']
+    list_display_links = ['id', 'name', 'email']
+    search_fields = ['name']
+    list_per_page = 25
+
+admin.site.register(UserAccount, UserAccountAdmin)
