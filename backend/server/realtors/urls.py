cd4406fef1a1a6737a368d42775278dc67ad3e6c
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-25 13:15:48 +0300

    implements routing for RealtorListView, RealtorView and TopSellerView

--- backend/server/realtors/urls.py
+++ backend/server/realtors/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import RealtorListView, RealtorView, TopSellerView
 
 urlpatterns = [
-    
+    path('', RealtorListView.as_view()),
+    path('<int:pk>/', RealtorView.as_view()),
+    path('topseller/', TopSellerView.as_view())
 ]
