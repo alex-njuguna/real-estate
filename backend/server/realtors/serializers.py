541f29062622879ffa2720c0106bfa8e858f8d1e
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-21 02:27:05 +0300

    a fresh start

--- backend/server/realtors/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Realtor
-
-class RealtorSerializer(serializers.ModelSerializer):
-    """serialize the Realtor model"""
-    class Meta:
-        model = Realtor
-        fields = '__all__'
