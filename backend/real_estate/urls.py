802b80c804583aa103c65c0cde78f077d95da9e1
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-22 02:28:17 +0300

    routing bug fix

--- backend/real_estate/urls.py
+++ backend/real_estate/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path(r"^.*",
+                        TemplateView.as_view(template_name='index.html'))]
