dc209de9c2be3a554ddf20b4747e8a74ac6990a3
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-23 01:13:28 +0300

    listings routes updates

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -3,7 +3,7 @@ from .views import ListingsView, ListingView, SearchView
 
 urlpatterns = [
     path('', ListingsView.as_view()),
-    path('<slug>', ListingView.as_view()),
+    path('<slug>/', ListingView.as_view()),
     path('search/', SearchView.as_view()),
 
 ]
