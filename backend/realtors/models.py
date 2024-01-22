c809438853cb9035e4cf328542c83a259ff2c9a0
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-22 03:32:37 +0300

    realtors model

--- backend/realtors/models.py
+++ backend/realtors/models.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Realtor(models.Model):
+    # data table for a realtor details
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    top_seller = models.BooleanField(default=False)
+    date_hired = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
