729fd155c8eb495627c69fd0c95b6c866c4da6e8
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-22 03:49:01 +0300

    display top seller functionality

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -17,3 +17,11 @@ class RealtorView(RetrieveAPIView):
     #  display a single realtor details
     queryset = Realtor.objects.all()
     serializer_class = RealtorSerializer
+
+
+class TopSellerView(ListAPIView):
+    # display the top seller
+    permission_classes = (permissions.AllowAny,)
+    queryset = Realtor.objects.filter(top_seller=True)
+    serializer_class = RealtorSerializer
+    pagination_class = None
