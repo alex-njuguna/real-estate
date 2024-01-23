e33710e3ce6056518305769eb9ad89565690fdc3
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-24 00:20:49 +0300

    api testing and adding some data

--- backend/contacts/admin.py
+++ backend/contacts/admin.py
@@ -6,7 +6,7 @@ from .models import Contact
 class ContactAdmin(admin.ModelAdmin):
     # customized look of the Contact model on the admin panel
     list_display = ['id', 'name', 'email', 'subject']
-    list_display_links = ['id', 'subject']
+    list_display_links = ['id', 'name', 'subject']
     search_fields = ['name', 'email', 'subject']
     list_per_page = 25
 
