622a56f584de175da46010256860ef4975200ca9
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-23 23:39:26 +0300

    mail setup for contacts

--- backend/real_estate/settings.py
+++ backend/real_estate/settings.py
@@ -148,3 +148,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 FILE_UPLOAD_PERMISSIONS = 0o640
 
 AUTH_USER_MODEL = 'account.UserAccount'
+
+
+# Email setup
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alexkienjeku@gmail.com'
+EMAIL_HOST_PASSWORD = 'judptnghbgrpqmnd'
+EMAIL_USE_TLS = True
