170838fcb930caf4f986e57452d8746f676ce81c
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-23 23:58:20 +0300

    send mail functionality

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core.mail import send_mail
 
-# Create your views here.
+from .models import Contact
+from .serializers import ContactSerializer
+
+
+class ContactCreateView(APIView):
+    # display platform for contact
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        # post an email
+        data = self.request.data
+
+        try:
+            send_mail(
+                data['subject'],
+                'Name: '
+                + data['name']
+                + '\nEmail: '
+                + data['email']
+                + '\n\nMessage:\n'
+                + data['message'],
+                'alexkienjeku@gmail.com',
+                ['alexkienjeku@gmail.com'],
+                fail_silently=False
+            )
+
+            contact = Contact(name=data['name'], email=data['email'],
+                              subject=data['subject'], message=data['message'])
+            contact.save()
+
+            return Response({'success': 'Message sent successfully'})
+
+        except:
+            return Response({'error': 'Message failed to send'})
