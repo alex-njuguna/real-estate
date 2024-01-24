5e60b11bfce8035acf1765080bb1704d158d7e01
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-24 17:13:36 +0300

    app routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./hocs/Layout";
+import {
+  About,
+  Contact,
+  Home,
+  ListingDetail,
+  Listings,
+  Signin,
+  Signup,
+} from "./containers/Containers";
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about/" element={<About />} />
+            <Route path="contact/" element={<Contact />} />
+            <Route path="listings/" element={<Listings />} />
+            <Route path="listings/:id" element={<ListingDetail />} />
+            <Route path="signin/" element={<Signin />} />
+            <Route path="signup/" element={<Signup />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/containers/Containers.js
@@ -0,0 +1,9 @@
+import About from './About'
+import Contact from './Contact'
+import Home from './Home'
+import ListingDetail from './ListingDetail'
+import Listings from './Listings'
+import Signin from './Signin'
+import Signup from './Signup'
+
+export {About, Contact, Home, ListingDetail, Listings, Signin, Signup}
