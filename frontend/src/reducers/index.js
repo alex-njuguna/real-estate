b277a0e90cdb196b9c191028daefd6767fd614fd
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-25 02:00:17 +0300

    creates alert action

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import Alert from './Alert'
-import Auth from './Auth'
-
+import Alert from "./Alert";
+import Auth from "./Auth";
 
 export default combineReducers({
-    Alert,
-    Auth
-})
-
+  Alert,
+  Auth,
+});
